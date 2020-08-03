a19f8cb3ab4dc02fe1d426c050dda39ba750b697
Riccardo Crepaldi <crepric@gmail.com>
2020-08-02 17:17:24 -0700

    Adding GNU license to all files

--- src/netctl/interface_checker.h
+++ src/netctl/interface_checker.h
@@ -1,3 +1,19 @@
+// This file is part of Net Failover Manager.
+//
+// Net Failover Manager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Net Failover Manager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Net Failover Manager.  If not, see
+// <https://www.gnu.org/licenses/>.
+
 #ifndef NET_FAILOVER_MANAGER_NETCTL_INTERFACE_CHECKER
 #define NET_FAILOVER_MANAGER_NETCTL_INTERFACE_CHECKER
 
