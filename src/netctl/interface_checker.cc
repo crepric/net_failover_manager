ae600b359a97b748b74cdec8f1e53412bbcac8e4
Riccardo Crepaldi <crepric@gmail.com>
2020-08-15 15:35:00 -0700

    Changing network health checks from ping return code to parsing the output of ping looking for packet loss information

--- src/netctl/interface_checker.cc
+++ src/netctl/interface_checker.cc
@@ -15,37 +15,103 @@
 // <https://www.gnu.org/licenses/>.
 
 #include "interface_checker.h"
+
 #include <glog/logging.h>
-#include <iostream>
-#include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <array>
+#include <boost/algorithm/string.hpp>
+#include <boost/tokenizer.hpp>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
 
 namespace net_failover_manager {
 
 namespace {
 
-const std::string kAddressToPing = "8.8.8.8"; // Google Public DNS.
-const int kPingTimeout = 4; // seconds, argument passed to the external ping.
-const std::chrono::duration kPingInterval = std::chrono::seconds(2);
+// Ping commands arguments.
+const std::string kAddressToPing = "8.8.8.8";  // Google Public DNS.
+const int kPingTimeout = 1;       // seconds, timeout to receive ping reply.
+const int kPingDuration = 3;      // seconds, duration of ping command.
+const float kPingInterval = 0.5;  // seconds, interval between pings.
+
+// Interval between two ping commands.
+const std::chrono::duration kIfCheckInterval = std::chrono::seconds(5);
+
+// Ping packet loss threshold for healthy interface;
+const int kPingPacketLossThreshold = 25;
+
+InterfaceChecker::InterfaceStatus ParsePingResult(
+    const std::string &ping_result, const std::string &if_name) {
+  boost::char_separator<char> fn("\n");
+  boost::tokenizer<boost::char_separator<char> > tokens(ping_result, fn);
+  for (auto str = tokens.begin(); str != tokens.end(); ++str) {
+    auto pos = (*str).find("packet loss");
+    if (pos != std::string::npos) {
+      // Line looks like this:
+      // 4 packets transmitted, 4 received, 0% packet loss, time 503ms
+      DLOG(INFO) << "Found stats line for interface " << if_name << ": "
+                 << *str;
+      std::string delimiters(",");
+      std::vector<std::string> stats_elements;
+      boost::split(stats_elements, *str, boost::is_any_of(delimiters));
+      for (auto element : stats_elements) {
+        // looking for the packet loss info.
+        auto pl_pos = element.find("% packet loss");
+        if (pl_pos != std::string::npos) {
+          int pl_value = 100;  // default to 100% packet loss.
+          try {
+            pl_value = stoi(element.substr(0, pl_pos));
+          } catch (const std::invalid_argument &ia) {
+            LOG(ERROR) << "Failed to convert packet loss to int value from "
+                       << element;
+            return InterfaceChecker::UNKNOWN;
+          }
+          DLOG(INFO) << "Packet loss for " << if_name << " recorded at "
+                     << pl_value;
+          if (pl_value > kPingPacketLossThreshold) {
+            LOG(WARNING) << "Packet loss for " << if_name
+                         << " higher than threshold, at " << pl_value;
+            return InterfaceChecker::UNHEALTHY;
+          } else {
+            return InterfaceChecker::HEALTHY;
+          }
+        }
+      }
+    }
+  }
+  return InterfaceChecker::UNKNOWN;
+}
+
+// Run a command in a subprocess and returns command output.
+std::string exec(const char *cmd) {
+  std::array<char, 256> buffer;
+  std::string output;
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+  if (!pipe) {
+    throw std::runtime_error("popen() failed!");
+  }
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    output += buffer.data();
+  }
+  return output;
+}
 
 // Tests interface connectivity by calling the external command ping.
 // TODO: change this to an internal ICMP sender.
 void TestPing(const std::string &interface,
               InterfaceChecker::InterfaceStatus *result) {
   std::stringstream command;
-  command << "ping " << kAddressToPing << " -c 1 -W " << kPingTimeout << " -I "
-          << interface << " > /dev/null";
+  command << "ping " << kAddressToPing << " -W " << kPingTimeout << " -w "
+          << kPingDuration << " -i " << kPingInterval << " -I " << interface;
   DLOG(INFO) << "calling " << command.str() << "\n";
-  int ping_result = system(command.str().c_str());
-  // Ping return status is 0 if ping successful.
-  if (ping_result == 0) {
-    *result = InterfaceChecker::HEALTHY;
-  } else {
-    *result = InterfaceChecker::UNHEALTHY;
-  }
+  auto ping_result = exec(command.str().c_str());
+  DLOG(INFO) << "Ping output:";
+  *result = ParsePingResult(ping_result, interface);
 }
-} // namespace
+}  // namespace
 
 InterfaceChecker::InterfaceChecker(const std::vector<std::string> &if_list,
                                    IfStatusChangedCallback status_changed_cb)
@@ -70,7 +136,7 @@ bool InterfaceChecker::StartChecks() {
             std::time_t timestamp = std::time(nullptr);
             std::chrono::system_clock::time_point next_check =
                 std::chrono::system_clock::now();
-            next_check += kPingInterval;
+            next_check += kIfCheckInterval;
             InterfaceStatus status;
             TestPing(interface_name, &status);
             {
@@ -135,4 +201,4 @@ bool InterfaceChecker::StopChecks() {
   return true;
 }
 
-} // namespace net_failover_manager
+}  // namespace net_failover_manager
